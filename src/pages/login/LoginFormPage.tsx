f608de90cac543904fbbdcbcf32e3a1fdda83815
PricedTuna <edgarevegar04@gmail.com>
2024-10-02 17:50:28 -0700

    handle the login form error (user not found)

--- src/pages/login/LoginFormPage.tsx
+++ src/pages/login/LoginFormPage.tsx
@@ -1,31 +1,36 @@
-import { useState } from 'react';
-import { Container, Box, TextField, Button, Typography } from '@mui/material';
-import { useNavigate } from 'react-router';
-import { useAuth } from '../../common/context/AuthContext';
-import { UserService } from '../../services/UserService';
-import { GetLoginUserDto } from '../../dtos/usuarios/GetLoginUserDto';
+import { useState } from "react";
+import { Container, Box, TextField, Button, Typography } from "@mui/material";
+import { useNavigate } from "react-router";
+import { useAuth } from "../../common/context/AuthContext";
+import { UserService } from "../../services/UserService";
+import { GetLoginUserDto } from "../../dtos/usuarios/GetLoginUserDto";
 
 function LoginForm() {
-  const [email, setEmail] = useState<string>('');
-  const [password, setPassword] = useState<string>('');
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const [isEmailError, setEmailError] = useState<boolean>(false);
+  const [isPasswordError, setPasswordError] = useState<boolean>(false);
+
   const navigate = useNavigate();
-  const {login} = useAuth();
+  const { login } = useAuth();
   const _userService = new UserService();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log('Email:', email);
-    console.log('Password:', password);
+    const userFound = (await _userService.login({
+      email: email,
+      password: password,
+    })) as GetLoginUserDto;
 
-    const userFound = await _userService.login({email: email, password: password}) as GetLoginUserDto;
-    if(userFound.idUsuario){
+    if (userFound) {
       login();
       navigate("/home");
-     } else {
-      // TODO: manejar correctamente el user not foundaut
+    } else {
       console.error("USER NOT FOUND");
-      console.log(JSON.stringify(userFound, null, 2)); // !
-     }
+      setEmailError(true);
+      setPasswordError(true);
+    }
   };
 
   return (
@@ -42,23 +47,31 @@ function LoginForm() {
         </Typography>
         <Box component="form" onSubmit={handleSubmit} width="100%">
           <TextField
+            error={isEmailError}
             label="Correo electrónico"
             variant="outlined"
             fullWidth
             margin="normal"
             type="email"
             value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={(e) => {
+              setEmail(e.target.value);
+              setEmailError(false);
+            }}
             required
           />
           <TextField
+            error={isPasswordError}
             label="Contraseña"
             variant="outlined"
             fullWidth
             margin="normal"
             type="password"
             value={password}
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={(e) => {
+              setPassword(e.target.value);
+              setPasswordError  (false);
+            }}
             required
           />
           <Button
