499a8f25c263bd67133ba731c41ce6e7e8027073
PricedTuna <edgarevegar04@gmail.com>
2024-10-28 01:06:43 -0700

    re-correccion

--- src/router/AdminRouter.tsx
+++ src/router/AdminRouter.tsx
@@ -2,32 +2,39 @@ import AutosPage from "../pages/admin/autos/AutosPage";
 import AutosListPage from "../pages/admin/autos/AutosListPage";
 import AutoCreatePage from "../pages/admin/autos/AutosCreatePage";
 import AdminPage from "../pages/admin/AdminPage";
+import MainLayout from "../layout/MainLayout";
 
 // Rutas para administradores
 export const AdminRouter = [
   {
-    path: "admin",
-    element: <AdminPage />,
+    path: "/",
+    element: <MainLayout />,
     children: [
       {
-        path: "autos",
-        element: <AutosPage />,
-        errorElement: <AutosPage />,
+        path: "admin",
+        element: <AdminPage />,
         children: [
           {
-            path: "",
-            element: <AutosListPage />,
-            errorElement: <AutosListPage />,
-          },
-          {
-            path: "crear",
-            element: <AutoCreatePage />,
-            errorElement: <AutoCreatePage />,
-          },
-          {
-            path: "editar",
-            element: <AutoCreatePage />,
-            errorElement: <AutoCreatePage />,
+            path: "autos",
+            element: <AutosPage />,
+            errorElement: <AutosPage />,
+            children: [
+              {
+                path: "",
+                element: <AutosListPage />,
+                errorElement: <AutosListPage />,
+              },
+              {
+                path: "crear",
+                element: <AutoCreatePage />,
+                errorElement: <AutoCreatePage />,
+              },
+              {
+                path: "editar",
+                element: <AutoCreatePage />,
+                errorElement: <AutoCreatePage />,
+              },
+            ],
           },
         ],
       },
